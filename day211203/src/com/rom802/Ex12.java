917bca557411feead1d8df686c7e47230158c263
Hernameis <msh4287@naver.com>
2021-12-03 17:04:45 +0900

    Ex13 done

---
+++ day211203/src/com/rom802/Ex12.java
@@ -0,0 +1,14 @@
+package com.rom802;
+
+public class Ex12 {
+	public static void main(String[] args) {
+		//
+		System.out.println("출력");
+		System.out.println();
+		System.out.print("\n");
+		System.out.print("출력1");
+		System.out.print("출력2\n");
+		System.out.print("출력3");
+		System.out.print("출력");
+	}
+}
