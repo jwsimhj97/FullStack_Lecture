6ce61e49899a79ccd74bf0e13b9847c9bb5993ae
Hernameis <msh4287@naver.com>
2021-12-06 17:04:04 +0900

    ex05 done

---
+++ day211206/src/com/rom802/Ex05.java
@@ -0,0 +1,13 @@
+package com.rom802;
+
+public class Ex05 {
+
+	public static void main(String[] args) {
+		for (int backNum=1; backNum<=9; backNum++) {
+			for (int frontNum=2; frontNum<=9; frontNum++) {
+				System.out.print(frontNum + " * " + backNum + " = " + frontNum*backNum + "\t");
+			}
+			System.out.println();
+		}
+	}
+}
